02162ddcbefe324da0dea2803820c41027f222ba
Austin White <basketballking19aw@gmail.com>
2022-03-21 17:06:49 -0600

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -1,6 +1,6 @@
 let gameComplete = false;
 // Define the three constants here
-const name = "unkown"
+const name = "unknown"
 const score = 0
 const items = 0
 // Define the player object here
@@ -37,14 +37,46 @@ const dateDiff = (date1, date2) => {
 
 // Here, use Object.defineProperty to create property - daysToExpire
 
+
+Object.defineProperty(Product.prototype, 'daysToExpire', {
+    get: function () {
+        return dateDiff(this.expiryDate, new Date());
+    }
+});
+
+
 // Add method getDetails to Product here
+Product.prototype.getDetails = function () {
+    return `Product Name: ${this.name} , Product Price: ${this.price}`;
+}
+
 
 // Define the MagicProduct class here
-
+function MagicProduct(id, name, price, expiryDate, points, isBonus) {
+    Product.call(this, id, name, price, expiryDate);
+    this.points = points;
+    this.isBonus = isBonus;
+}
 // Establish inheritance between Product() & MagicProduct() here
-
+MagicProduct.prototype = Object.create(Product.prototype);
 // Define Rating class here
+class Rating {
+    constructor() {
+        this.rate = "";
+    }
 
+    set rating(value) {
+        if (value > 1 && value <= 4) {
+            this.rate = "OK";
+        } else if (value >= 5 && value <= 7) {
+            this.rate = "GOOD";
+        } else if (value > 7) {
+            this.rate = "EXCEPTIONAL";
+        } else {
+            this.rate = "BAD";
+        }
+    }
+}
 // Complete the loadProducts function
 const loadProducts = (map, prodId) => {
     let a = new Array();
