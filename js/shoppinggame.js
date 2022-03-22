2ad296360f02c37f9547bec00a14676553c2c67d
Austin White <basketballking19aw@gmail.com>
2022-03-22 14:04:49 -0600

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -372,6 +372,7 @@ function init(data) {
                 rateAndExit();
             } else {
                 // Call rating setter method of playerRating to set user entered rate value here
+                playerRating.rating = r;
                 
                 // Call Object.assign() method here to populate target
                 
